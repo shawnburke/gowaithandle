186958b1c9e8ac77f57630af2e16a16e726d8c0b
Shawn Burke <shawn@shawnburke.com>
2022-09-24 21:39:53 -0700

    add WaitAny test

--- helpers_test.go
+++ helpers_test.go
@@ -30,6 +30,31 @@ func TestWaitAll(t *testing.T) {
 	require.Equal(t, 1, int(count))
 }
 
+func TestWaitAny(t *testing.T) {
+
+	mre1 := &ManualResetEvent{}
+	mre2 := &ManualResetEvent{}
+
+	count := int32(0)
+	done := make(chan struct{})
+	go func() {
+		result := <-WaitAny(testTimeoutContext(time.Second), mre1, mre2)
+		require.True(t, result)
+		atomic.StoreInt32(&count, 1)
+		close(done)
+	}()
+
+	mre1.Set()
+	<-done
+	require.Equal(t, 1, int(count))
+	mre1.Reset()
+
+	// test timeout
+	res := <-WaitAny(testTimeoutContext(time.Millisecond), mre1, mre2)
+	require.False(t, res)
+
+}
+
 type contextKey string
 
 func testTimeoutContext(timeout time.Duration) context.Context {
